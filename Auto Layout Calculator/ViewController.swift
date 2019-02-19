05081fde47b235ca3821032c376be186d1c5a778
Paulo Andre Melo Costa <pauloamcosta2@gmail.comgit>
2019-02-19 00:45:30 -0300

    bulding layout using stack view

---
+++ Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Paulo Costa on 18/02/19.
+//  Copyright © 2019 Paulo Costa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+    }
+    
+}
+
---
+++ README.md
@@ -0,0 +1,8 @@
+# Auto-Layout-Practice
+
+Auto layout practice - Building one calculator using stack views
+
+Exercise pro the iOS bootcamp developer course.
+
+![Screenshot](screenshot.png)
+
